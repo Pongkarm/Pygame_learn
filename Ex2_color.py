82b8d4f03cb7afdc27830ce234919f03669577ed
pongkarm <darunpoppong@gmail.com>
2025-01-19 17:34:01 +0700

    add Ex2

---
+++ Ex2_color.py
@@ -0,0 +1,33 @@
+#การกำหนดสีจะกำหนดแบบ RGB เป็นรูปแบบ Tuple (0,0,0) (R,G,B)
+#ชื่อตัวแปร = (R,G,B)
+import time
+import pygame
+pygame.init()
+pygame.display.set_caption("Game's Pongkarm")
+
+#กำหนดขนดหน้าจอ จะใช้ตัวแปรพิมใหญ่ทั้งหมดสำหรับค่าคงที่ที่ไม่มีการเปลี่ยนแปลง
+SCREEN_W = 1000
+SCREEN_H = 800
+
+screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+
+#ตั้งค่าสี
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 255)
+RED = (255, 0, 0)
+
+screen.fill(RED)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.display.update()   
+    # time.sleep(0.5)
+    # screen.fill(BLACK)
+    # pygame.display.update()   
+    # time.sleep(0.5)
+    # screen.fill(RED)
+pygame.quit()
